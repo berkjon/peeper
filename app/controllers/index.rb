28f522400b1e10eb20fb6577c32cea051ce6a427
Jonathan Berk & EclecticKlos <pair+berkjon.eclecticklos@devbootcamp.com>
2015-03-19 12:15:13 -0700

    skeleton for homepage with signup and login post routes and skeleton index.erb

---
+++ app/controllers/index.rb
@@ -0,0 +1,43 @@
+get '/' do
+  #check if user is logged in
+  # session[:user_id] = nil
+  if session[:user_id]
+    @current_user = User.find(id: session[:user_id])
+    erb :feed
+  else
+    erb :index
+  end
+end
+
+post '/signup' do
+  p params
+  new_user = User.new(params)
+
+  if new_user.save
+    session[:user_id] = new_user.id
+    redirect '/'
+  else
+    #show errors
+    p "fix errors"
+  end
+
+end
+
+post '/login' do
+  p params
+  if User.where(username: params[:username].first
+    current_user = User.where(username: params[:username].first
+    if current_user.password == params[:password]
+      session[:user_id] = current_user.id
+      redirect '/'
+    else
+      #show incorrect password error
+      p "sorry, wrong password"
+    end
+  else
+    #say user doesn't exist
+    p "sorry, that username doesn't exist"
+  end
+end
+
+\
